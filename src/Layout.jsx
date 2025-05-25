fe2dc1a193bdf4b4f4a24348c7ac7d00ef13f08b
anumeetkumar <anumeetkumar@gmail.com>
2025-05-25 18:24:35 +0530

    refactor(Layout): remove unused imports and clean up div structure

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div >
-      
+    <div>
       <div className="min-h-screen">{children}</div>
- 
     </div>
   );
 };
