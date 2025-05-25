284f0cc5dfaab081738817fc29235fc1d3734e09
anumeetkumar <anumeetkumar@gmail.com>
2025-05-25 18:27:45 +0530

    fix: correct import casing for Layout component

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
-import Layout from "./layout.jsx";
+import Layout from "./Layout.jsx"; // Reverted to original casing based on directory tree
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
